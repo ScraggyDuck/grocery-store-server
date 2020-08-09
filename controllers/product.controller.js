9291093d3e9a2ab127751bbea8848b5cf34a02e8
JokerV <Quocvietcoi2000@gmail.com>
2020-08-09 19:33:31 +0700

    get products by query

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,13 +3,28 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.getAll = catchAsync(async (req, res, next) => {
-  let products = await Product.find();
-  res.status(201).json({
-    status: 'success',
-    data: {
-      data: products,
-    },
-  });
+  const { limit, category, keyword } = req.query;
+  //Tim tat cac cac san pham co chua keyword
+  let regex = new RegExp(keyword, 'i');
+  if (limit) {
+    let products = [];
+    if (!category && !keyword) {
+      products = await Product.find().limit(parseInt(limit));
+    } else {
+      let filter = category
+        ? { categories: [category], name: regex }
+        : { name: regex };
+      products = await Product.find(filter).limit(parseInt(limit));
+    }
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: products,
+      },
+    });
+  } else {
+    return next(new AppError('Can not get products!', 404));
+  }
 });
 
 exports.createProduct = catchAsync(async (req, res, next) => {
